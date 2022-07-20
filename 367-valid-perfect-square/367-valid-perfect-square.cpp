36627cbbe7e8e93384e0402df5ddecfe841eada7
Nishant Giri <106369419+raptor4444@users.noreply.github.com>
2022-07-20 22:14:18 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (64.45%) - LeetHub

---
+++ 367-valid-perfect-square/367-valid-perfect-square.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        for(long i=1; i<=num; i++){
+            
+            if(i*i == num) return true;
+            if(i*i > num) return false;
+            
+        }
+        return false;
+    }
+};
