938d151ef17b0d172c7d0573571e75ea3e53ea75
Nishant Giri <106369419+raptor4444@users.noreply.github.com>
2022-08-20 00:01:36 +0530

    Time: 22 ms (47.89%), Space: 12.9 MB (14.66%) - LeetHub

--- 2248-intersection-of-multiple-arrays/2248-intersection-of-multiple-arrays.cpp
+++ 2248-intersection-of-multiple-arrays/2248-intersection-of-multiple-arrays.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
     vector<int> intersection(vector<vector<int>>& nums) {
-        int n=nums.size();
+        int k=nums.size();
         map<int,int>m;
-        for(int i=0;i<nums.size();i++){
+        for(int i=0;i<k;i++){
             vector<int>n=nums[i];
-            for(int j=0;j<n.size();j++){
+            int p = n.size();
+            for(int j=0;j<p;j++){
                 m[n[j]]++;
             }
         }
         vector<int>v=nums[0];
         vector<int>V;
-        for(int i=0;i<v.size();i++){
-            if(m[v[i]]==nums.size()){
+        int l = v.size();
+        for(int i=0;i<l;i++){
+            if(m[v[i]]==k){
                 V.push_back(v[i]);
             }
         }
